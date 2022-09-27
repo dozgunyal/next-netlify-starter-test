50621b1541b62b3b1d793ac52e1cbbf903a37896
Özgün Yalcin <oezguen.yalcin@check24.de>
2022-09-27 10:56:23 +0200

    another type of redirect

--- pages/redirect.js
+++ pages/redirect.js
@@ -3,13 +3,8 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 import { useState } from 'react'
 
-const redirect = (link) => {
+const onClick = (link) => {
   window.location.href = link;
-}
-
-
-const redirectWith302 = (link) => {
-  window.location.href = `/redirect?url=${link}`;
 }
 
 export default function Home() {
@@ -19,18 +14,27 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Redirector" />
         <input type="text" onChange={(e) => setLink(e.target.value)}/>
         {link}
-        <button onClick={() => redirect(link)}>Redirect</button>
-        <button onClick={() => redirect(link)}>Redirect using 302</button>
+        <button onClick={() => onClick(link)}>Redirect</button>
       </main>
 
       <Footer />
     </div>
   )
 }
+
+// This function gets called at build time
+export async function getServerSideProps({query}) {
+  return {
+    redirect: {
+      destination: query.url,
+      statusCode: 302
+    },
+  }
+}
+
